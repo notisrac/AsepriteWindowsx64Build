6dfb0cd5b24c37056e3e0f6f93b615ef32defd45
notisrac <notisrac@gmail.com>
2020-10-02 20:34:07 +0200

    Modified the build.ps1, it checks if the prerequisites are already met (cloned repo, downloaded skia, build dir created), skips these steps

--- assets/build.ps1
+++ assets/build.ps1
@@ -14,50 +14,64 @@ $start_time = Get-Date
 # get the aseprite repo
 write-header "Cloning aseptire repo"
 
-$latestTag = ""
-if ($null -eq $args[0]) {
-    Write-Output "Getting the latest tag:"
-    $latestTag = (git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' $url '*.*' | Select-Object -Last 1).split('')[1] -replace "refs/tags/",""
-    Write-Output "  $latestTag"
-}
-elseif ($args[0] -eq "master") {
-    Write-Output "Using master"
-    $latestTag = "master"
+# check if the repo is already cloned
+if ((Test-Path $env:ASEPRITE_REPO) -eq $false) {
+    $latestTag = ""
+    if ($null -eq $args[0]) {
+        Write-Output "Getting the latest tag:"
+        $latestTag = (git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' $url '*.*' | Select-Object -Last 1).split('')[1] -replace "refs/tags/",""
+        Write-Output "  $latestTag"
+    }
+    elseif ($args[0] -eq "master") {
+        Write-Output "Using master"
+        $latestTag = "master"
+    }
+    else {
+        $latestTag = $args[0]
+    }
+    
+    # cloning the repo
+    #Start-Process -FilePath "git" -ArgumentList "clone", "--recursive", $url -NoNewWindow -Wait
+    git clone --recursive -b $latestTag --single-branch $url $env:ASEPRITE_REPO
 }
 else {
-    $latestTag = $args[0]
+    Write-Output "Already cloned..."
 }
 
-# cloning the repo
-#Start-Process -FilePath "git" -ArgumentList "clone", "--recursive", $url -NoNewWindow -Wait
-git clone --recursive -b $latestTag --single-branch $url $env:ASEPRITE_REPO
 
 write-header "Getting the Skia pre-built binary"
-$skia = Invoke-WebRequest 'https://api.github.com/repos/aseprite/skia/releases/latest' -UseBasicParsing | Convertfrom-json
-$assetUrl = $null
-$assetName = $null
-foreach ($asset in $skia.assets) {
-    if ($asset.name -like '*Windows-Release-x64*') {
-        $assetUrl = $asset.browser_download_url
-        $assetName = $asset.name
+if ((Test-Path $env:ASEPRITE_DEPS) -eq $false) {
+    $skia = Invoke-WebRequest 'https://api.github.com/repos/aseprite/skia/releases/latest' -UseBasicParsing | Convertfrom-json
+    $assetUrl = $null
+    $assetName = $null
+    foreach ($asset in $skia.assets) {
+        if ($asset.name -like '*Windows-Release-x64*') {
+            $assetUrl = $asset.browser_download_url
+            $assetName = $asset.name
+        }
     }
+    
+    if ($null -eq $assetUrl) {
+        throw "Could not find a windows x64 release!"
+    }
+    
+    # download skia
+    $output = "$($env:ASEPRITE_TEMP)\$assetName"
+    (New-Object System.Net.WebClient).DownloadFile($assetUrl, $output)
+    
+    New-Item -Path $env:ASEPRITE_DEPS -Name "skia" -ItemType "directory" | Out-Null
+    
+    $skiaLocation = "$($env:ASEPRITE_DEPS)\skia"
+    # extract skia into the deps folder
+    Expand-Archive -LiteralPath $output -DestinationPath $skiaLocation
+}
+else {
+    Write-Output "Already downloaded..."
 }
 
-if ($null -eq $assetUrl) {
-    throw "Could not find a windows x64 release!"
+if ((Test-Path "$($env:ASEPRITE_REPO)\build") -eq $false) {
+    New-Item -Path $env:ASEPRITE_REPO -Name "build" -ItemType "directory" | Out-Null
 }
-
-# download skia
-$output = "$($env:ASEPRITE_TEMP)\$assetName"
-(New-Object System.Net.WebClient).DownloadFile($assetUrl, $output)
-
-New-Item -Path $env:ASEPRITE_DEPS -Name "skia" -ItemType "directory" | Out-Null
-
-$skiaLocation = "$($env:ASEPRITE_DEPS)\skia"
-# extract skia into the deps folder
-Expand-Archive -LiteralPath $output -DestinationPath $skiaLocation
-
-New-Item -Path $env:ASEPRITE_REPO -Name "build" -ItemType "directory" | Out-Null
 Set-Location "$($env:ASEPRITE_REPO)\build"
 
 write-header "Running cmake"
